842ae6a709d2275e3f4aed6bcde844ceb24aba6a
IBOPECK <kaldun597@gmail.com>
2025-04-15 12:08:17 +0500

    Update Student.cs
    базе вахт мешава ки маълумота ворид накни хатоги намоиш мета барои ҳами нул ворид мешава

--- Domain/Student.cs
+++ Domain/Student.cs
@@ -2,7 +2,11 @@ namespace Domain;
 
 public class Student
 {
+    [Key]
     public int Id { get; set; }
-    public string Name { get; set; }
+    
+    public string? Name { get; set; } = null;
+    
     public int GroupId { get; set; }
+    
 }
